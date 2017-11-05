ee3355b38f1d94f586caf6510212c7392a1b27a8
AlexMalishev <A-lexing@mail.ru>
2017-11-05 21:40:57 +0300

    New classes and adding del to classes

--- GUI.py
+++ GUI.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
 
 import sys,my,time,math
-from PyQt5.QtGui import (QFont, QIcon, QPaintDevice,QPen,QBrush, QColor, QPainter)
+from PyQt5.QtGui import (QFont, QIcon, QPaintDevice,QPen,QBrush, QColor, QPainter,QPalette)
 from PyQt5.QtCore import (QEvent,QLine, QPoint, QRectF, Qt,pyqtSignal)
 from PyQt5.QtWidgets import (QApplication,QWidget, QGraphicsView, QGraphicsScene, QGraphicsItem,
                              QGridLayout, QVBoxLayout, QHBoxLayout,
                              QLabel, QLineEdit, QPushButton)
+class Point:
+    def __init__(self,x=0,y=0):
+        self.X=x
+        self.Y=y
+    def getX(self):
+        return self.X
+    def getY(self):
+        return self.Y
+    def setX(self,x):
+        self.X=x
+    def setY(self,y):
+        self.Y=y
 
+class Line(Point):
+
+    def __init__(self):
+        self.P1=None
+        self.P2=None
+    def setP1(self,P1):
+        self.P1=P1
+    def setP2(self,P2):
+        self.P2=P2
+    def x1(self):
+        return self.P1.getX()
+    def y1(self):
+        return self.P1.getY()
+    def x2(self):
+        return self.P2.getX()
+    def y2(self):
+        return self.P2.getY()
 class MyWin(QWidget):
     def __init__(self,parent=None):
         QWidget.__init__(self,parent)
@@ -21,7 +50,12 @@ class MyWin(QWidget):
         self.ui.pushButton.clicked.connect(self.createP)
         self.ui.pushButton_2.clicked.connect(self.createI)
 
+        self.ui.pushButtonDel.clicked.connect(self.delP)
+        self.ui.pushButtonDel_2.clicked.connect(self.delI)
 
+
+        self.ui.pushButton_9.clicked.connect(self.angle)
+        self.ui.pushButton_10.clicked.connect(self.lenght)
 
         self.scene=QGraphicsScene()
         self.ui.graph.setScene(self.scene)
@@ -33,26 +67,80 @@ class MyWin(QWidget):
         self.points=list()
         self.lines=list()
         self.InteFlag=0
+        self.delIflag=0
+        self.delPflag=0
         self.Linepenpen=QPen(QColor('#000000'),2)
         self.pointpen = QPen(QColor('#ff0000'), 2)
         self.Linepenpen.style="DashLine"
         self.Linepenpen.cap="RoundCap"
+
+    def angle(self):
+        self.createmodal('Угол')
+    def lenght(self):
+        self.createmodal('Длина')
+
+    def createmodal(self,t):
+        modalWindow=QWidget(window,Qt.Window)
+        modalWindow.setWindowTitle(t)
+        modalWindow.resize(250,40)
+        modalWindow.setWindowModality(Qt.WindowModal)
+        modalWindow.setAttribute(Qt.WA_DeleteOnClose,True)
+        modalWindow.move(window.geometry().center() - modalWindow.rect().center() -QPoint(4,30))
+        modLab=QLabel(t)
+        modBut=QPushButton('Применить')
+        lineEdit=QLineEdit()
+        hBox=QHBoxLayout()
+        hBox.addWidget(modLab)
+        hBox.addWidget(lineEdit)
+        hBox.addWidget(modBut)
+        modalWindow.setLayout(hBox)
+        modBut.clicked.connect(modalWindow.close)
+        modalWindow.show()
+
+
+
     def eventFilter(self, source, event):
 
         if (event.type() == QEvent.MouseButtonPress and
                     source is self.ui.graph):
             if self.PointFlag==1:
+
                 pos = event.pos()
                 print('add point: (%d, %d)' % (pos.x(), pos.y()))
-                self.points.append(QPoint(pos))
+                self.points.append(Point(pos.x(),pos.y()))
                 self.PointFlag=0
-                self.scene.addEllipse(pos.x()-2.5,pos.y()-2.5,5,5,self.pointpen)
-                self.scene.update()
+                self.painter()
+
+
+            if self.delPflag==1:
+                pos = event.pos()
+                poi=self.getpoint(event.pos())
+                if poi != None:
+                    allline=list()
+                    for line in self.lines:
+                        if line.P1==poi or line.P2==poi:
+                            allline.append(line)
+                    for line in allline:
+                        self.lines.remove(line)
+                    self.points.remove(poi)
+                    self.delPflag=0
+                    self.painter()
+
+
+            if self.delIflag==1:
+                pos = event.pos()
+                lin = self.getline(event.pos())
+                if lin != None:
+                    self.lines.remove(lin)
+
+                    self.delIflag=0
+                    self.painter()
             if self.InteFlag>0:
                 if self.InteFlag==2:
 
-                    lin=QLine()
+                    lin=Line()
                     if self.getpoint(event.pos())!=None:
+
                         lin.setP1(self.getpoint(event.pos()))
                         self.lines.append(lin)
                     else:
@@ -61,20 +149,45 @@ class MyWin(QWidget):
                     if self.getpoint(event.pos()) != None:
                         self.lines[-1].setP2(self.getpoint(event.pos()))
                         print('add line()',self.lines[-1].x1(),self.lines[-1].y1(),self.lines[-1].x2(),self.lines[-1].y2())
-                        self.scene.addLine(self.lines[-1].x1(),self.lines[-1].y1(),self.lines[-1].x2(),self.lines[-1].y2(),self.Linepenpen)
-                        self.scene.update()
+                        self.painter()
                     else:
                         self.InteFlag+=1
                 self.InteFlag-=1
+
         return QWidget.eventFilter(self, source, event)
+
+
+    def painter(self):
+        self.scene.clear()
+        self.grid(13)
+        for point in self.points:
+            self.scene.addEllipse(point.X - 2.5, point.Y - 2.5, 5, 5, self.pointpen)
+        for line in self.lines:
+            self.scene.addLine(line.x1(), line.y1(), line.x2(), line.y2(),self.Linepenpen)
+        self.scene.update()
+
+
     def getpoint(self,a):
         x=a.x()
         y=a.y()
-        tolerance=15
+        tolerance=10
         for point in self.points:
-            if abs(point.x()-x)<tolerance and abs(point.y()-y)<tolerance:
+            if abs(point.getX()-x)<tolerance and abs(point.getY()-y)<tolerance:
                 return point
         return None
+
+    def getline(self,a):
+        x = a.x()
+        y = a.y()
+        tolerance = 5
+        for line in self.lines:
+            t=abs(y-(line.y1()+((line.y2()-line.y1())*(x-line.x1())/(line.x2()-line.x1()))))
+            print(t)
+            if t<tolerance:
+                return line
+        return None
+
+
     def grid(self,dxy):
         pen=QPen()
         pen.setColor(QColor('#C0C0C0'))
@@ -84,15 +197,41 @@ class MyWin(QWidget):
             self.scene.addLine( dxy*i,0, dxy*i, self.ui.graph.height(),pen)
 
 
+    def delP(self):
+        self.delPflag=1
+
+        self.delIflag = 0
+        self.PointFlag = 0
+        self.InteFlag = 0
+
+
+    def delI(self):
+        self.delIflag = 1
+
+        self.delPflag = 0
+        self.PointFlag = 0
+        self.InteFlag = 0
+
     def createP(self):
         self.PointFlag=1
 
+        self.delPflag = 0
+        self.delIflag = 0
+        self.InteFlag = 0
+
     def createI(self):
         self.InteFlag=2
+
+        self.delPflag = 0
+        self.delIflag = 0
+        self.PointFlag = 0
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     window=MyWin()
+    pal = window.palette()
+    pal.setColor(QPalette.Normal, QPalette.Window, QColor("#7fffd4"))
+    window.setPalette(pal)
     window.show()
     sys.exit(app.exec())
--- my.py
+++ my.py
@@ -7,7 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon,QPalette,QColor
 from PyQt5.QtCore import  Qt
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -26,13 +26,27 @@ class Ui_MainWindow(object):
         self.groupBox = QtWidgets.QGroupBox(self.frame)
         self.groupBox.setGeometry(QtCore.QRect(10, 50, 141, 101))
         self.groupBox.setObjectName("groupBox")
+
         self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_2.setGeometry(QtCore.QRect(20, 20, 91, 31))
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 20, 51, 31))
         self.pushButton_2.setObjectName("pushButton_2")
+
+        self.pushButtonDel_2 = QtWidgets.QPushButton(self.groupBox)
+        self.pushButtonDel_2.setGeometry(QtCore.QRect(73, 20, 51, 31))
+        self.pushButtonDel_2.setObjectName("pushButton_2")
+
         self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(20, 60, 91, 31))
+        self.pushButton.setGeometry(QtCore.QRect(10, 60, 51, 31))
         self.pushButton.setMouseTracking(False)
         self.pushButton.setObjectName("pushButton")
+
+        self.pushButtonDel = QtWidgets.QPushButton(self.groupBox)
+        self.pushButtonDel.setGeometry(QtCore.QRect(73, 60, 51, 31))
+        self.pushButtonDel.setMouseTracking(False)
+        self.pushButtonDel.setObjectName("pushButton")
+
+
+
         self.groupBox_2 = QtWidgets.QGroupBox(self.frame)
         self.groupBox_2.setGeometry(QtCore.QRect(10, 200, 141, 241))
         self.groupBox_2.setObjectName("groupBox_2")
@@ -103,6 +117,22 @@ class Ui_MainWindow(object):
         self.pushButton_8.setIcon(QIcon('./pict/pp.png'))
         self.pushButton_9.setIcon(QIcon('./pict/angle.png'))
         self.pushButton_10.setIcon(QIcon('./pict/lenght.png'))
+        self.style="""
+            QPushButton:hover { background-color: red }
+            QPushButton:!hover { background-color: white }
+
+            QPushButton:pressed { background-color: rgb(0, 255, 0); }
+        """
+
+        self.pushButton_3.setStyleSheet(self.style)
+        self.pushButton_4.setStyleSheet(self.style)
+        self.pushButton_5.setStyleSheet(self.style)
+        self.pushButton_6.setStyleSheet(self.style)
+        self.pushButton_7.setStyleSheet(self.style)
+        self.pushButton_8.setStyleSheet(self.style)
+        self.pushButton_9.setStyleSheet(self.style)
+        self.pushButton_10.setStyleSheet(self.style)
+
 
         self.graph.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.graph.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -114,8 +144,10 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "GeoSolve"))
         self.groupBox.setTitle(_translate("MainWindow", "Примитивы"))
-        self.pushButton_2.setText(_translate("MainWindow", "Интервал"))
+        self.pushButton_2.setText(_translate("MainWindow", "Отрезок"))
         self.pushButton.setText(_translate("MainWindow", "Точка"))
+        self.pushButtonDel_2.setText(_translate("MainWindow", "Удалить"))
+        self.pushButtonDel.setText(_translate("MainWindow", "Удалить"))
         self.groupBox_2.setTitle(_translate("MainWindow", "Ограничения"))
         self.menufh.setTitle(_translate("MainWindow", "Файл"))
         self.menuAbout.setTitle(_translate("MainWindow", "About"))
